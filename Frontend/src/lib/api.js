d84de461e29b9d2c7f8ebc5883b897e4a836ef70
Mohd Rizwan <245871374+Mohd-Rizwan17@users.noreply.github.com>
2026-03-31 21:12:08 +0530

    update api

--- Frontend/src/lib/api.js
+++ Frontend/src/lib/api.js
@@ -1,8 +1,19 @@
+// import axios from "axios";
+
+// const baseURL =
+//   import.meta.env.VITE_API_BASE_URL ||
+//   (import.meta.env.DEV ? "http://localhost:3000" : "");
+
+// const api = axios.create({
+//   baseURL,
+//   withCredentials: true,
+// });
+
+// export default api;
+
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.VITE_API_BASE_URL ||
-  (import.meta.env.DEV ? "http://localhost:3000" : "");
+const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
 
 const api = axios.create({
   baseURL,
